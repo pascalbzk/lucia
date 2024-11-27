90719ce77b0acf5d476e0a1f6d40b6212f1a0e57
pascalbzk <pascalbuzake@gmail.com>
2024-11-27 18:52:50 +0845

    Update shop.php

--- Connections/shop.php
+++ Connections/shop.php
@@ -3,8 +3,8 @@
 # Type="MYSQL"
 # HTTP="true"
 $hostname_shop = "localhost";
-$database_shop = "shopping";
-$username_shop = "root";
-$password_shop = "";
+$database_shop = "id22069323_shopping";
+$username_shop = "id22069323_root";
+$password_shop = "847202611@Gmail.com";
 $shop = mysqli_connect($hostname_shop, $username_shop, $password_shop, $database_shop) or trigger_error(mysqli_error(),E_USER_ERROR); 
 ?>
